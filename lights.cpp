d993ee57a02dc14e238329a0ea94d985438b81d5
Tom <tom@tom-OptiPlex-960.(none)>
2013-02-07 13:47:42 -0800

    Added some more code for Lights and Light.

--- lights.cpp
+++ lights.cpp
@@ -3,4 +3,23 @@
 Lights::Lights(Scene *parent)
 {
     theScene = parent;
+    lights[0].lightSwitch = Light::ON;
+    lights[0].location = Light::LOCAL;
+}
+
+Lights::~Lights()
+{
+    theScene = nullptr;
+}
+
+Lights::Light::Light()
+{
+    lightSwitch = OFF;
+    spotLight = false;
+    spotCutoff = 180.0f;
+    spotExponent = 0.0f;
+    constAttenuation = 1.0f;
+    linearAttenuation = 0.0f;
+    quadraticAttenuation = 0.0f;
+    location = LOCAL;
 }
--- lights.h
+++ lights.h
@@ -6,8 +6,36 @@ class Scene;
 class Lights
 {
 public:
-    Lights(Scene *parent);
+    class Light
+    {
+    public:
+        Light();
 
+        const static int OFF = 0;
+        const static int ON = 1;
+        const static int DIRECTIONAL = 0;
+        const static int LOCAL = 1;
+        int lightSwitch;
+        float ambient[4] = {0.3f, 0.3f, 0.3f, 1.0f};
+        float diffuse[4] = {0.5f, 0.5f, 0.5f, 1.0f};
+        float specular[4] = {1.0f, 1.0f, 1.0f, 1.0f};
+        float position[4] = {0.0f, 0.0f, 200.0f, 1.0f};
+        float viewPos[4] = {0.0f, 0.0f, 0.0f, 0.0f};
+        float direction[4] = {0.0f, 0.0f, -1.0f, 0.0f};
+        float spotCutoff;
+        float spotExponent;
+        float spotDirection[3] = {0.0f, 0.0f, -1.0f};
+        float constAttenuation;
+        float linearAttenuation;
+        float quadraticAttenuation;
+        bool spotLight;
+        int location;
+    };
+
+    Lights(Scene *parent);
+    virtual ~Lights();
+
+    Light lights[8];
     Scene *theScene;
 };
 
